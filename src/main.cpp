80c388979e29ff8df717d46226a268bfa762d630
Charles BUlcão <charles@mx>
2020-11-11 21:27:19 -0300

    inicializando a classe animal

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "animal.hpp"
 
 int main(/*int argc, char* argv[]*/)
 {
 
 	std::cout << "Projeto LP1" << std::endl;
+
+	Animal cachorro(1,nativo,false,"125");
+
+	cachorro.printAnimal();
 
 	return 0;
 }
